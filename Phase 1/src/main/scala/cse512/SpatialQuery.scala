eae8db86a6df511c93c5a056eb29ead77c9227f2
Omar Alnoman <oalnoman@asu.edu>
2021-04-11 11:05:47 -0700

    small code refactoring

--- Phase 1/src/main/scala/cse512/SpatialQuery.scala
+++ Phase 1/src/main/scala/cse512/SpatialQuery.scala
@@ -5,47 +5,47 @@ import org.apache.spark.sql.SparkSession
 object SpatialQuery extends App{
   def ST_Contains(queryRectange: String, pointString: String): Boolean = {
 
-    // Parse Point Query and Convert to Double
-    val point = pointString.split(",").map(_.toDouble)
+    // Parse the pontString by comma into an array of x and y coordinate of type double
+    val pointDouble = pointString.split(",").map(_.toDouble)
 
-    // Store x and y coordinates of point
-    val point_x = point(0)
-    val point_y = point(1)
+    // Store x and y coordinates of point into separate variables
+    val x_point = pointDouble(0)
+    val y_point = pointDouble(1)
 
-    // Parse queryRectangle and Convert to Double
+    // Parse queryRectangle by comma into an array of type double
     val rectangle = queryRectange.split(",").map(_.toDouble)
 
-    // Store x and y coordinates of rectangle
-    val rect_x1 = Math.min(rectangle(0), rectangle(2))
-    val rect_y1 = Math.min(rectangle(1),rectangle(3))
-    val rect_x2 = Math.max(rectangle(0), rectangle(2))
-    val rect_y2 = Math.max(rectangle(1),rectangle(3))
+    // Find which point is the lower left (x1,y1) and which is the upper right (x2,y2) and store them
+    val x1_rectangle = Math.min(rectangle(0), rectangle(2))
+    val y1_rectangle = Math.min(rectangle(1),rectangle(3))
+    val x2_rectangle = Math.max(rectangle(0), rectangle(2))
+    val y2_rectangle = Math.max(rectangle(1),rectangle(3))
 
-    // Check if point is within the rectangle
-    return (point_x >= rect_x1 && point_x <= rect_x2 && point_y >= rect_y1 && point_y <= rect_y2)
+    // check if the rectangle contains the point.
+    (x_point >= x1_rectangle) && (x_point <= x2_rectangle) && (y_point >= y1_rectangle) && (y_point <= y2_rectangle)
   }
 
   def ST_Within(pointString1: String, pointString2: String, distance: Double): Boolean = {
 
-    // Parse point1 and Convert to Double
-    val point1 = pointString1.split(",").map(_.toDouble)
+    // Parse the pontString1 by comma into an array of x and y coordinate of type double
+    val pointDouble1 = pointString1.split(",").map(_.toDouble)
 
-    // Store x and y coordinates ofpoint1
-    val p1_x = point1(0)
-    val p1_y = point1(1)
+    // Store x and y coordinates of pointDouble1 into separate variables
+    val x_point1 = pointDouble1(0)
+    val y_point1 = pointDouble1(1)
 
-    // Parse point2 and Convert to Double
-    val point2 = pointString2.split(",").map(_.toDouble)
+    // Parse the pontString1 by comma into an array of x and y coordinate of type double
+    val pointDouble2 = pointString2.split(",").map(_.toDouble)
 
-    // Store x and y coordinates of point2
-    val p2_x = point2(0)
-    val p2_y = point2(1)
+    // Store x and y coordinates of pointDouble2 into separate variables
+    val x_point2 = pointDouble2(0)
+    val y_point2 = pointDouble2(1)
 
-    // ((p1_x - p2_x)^2 + (p1_y - p2_y)^2)^0.5
-    val e_dist = Math.sqrt( Math.pow((p1_x-p2_x),2) + Math.pow((p1_y-p2_y),2) )
+    // Find the Euclidean distance between two points sqrt((x_point1 - x_point2)^2 + (y_point1 - y_point2)^2)
+    val calculated_dist = Math.sqrt( Math.pow((x_point1-x_point2),2) + Math.pow((y_point1-y_point2),2) )
 
-    // Return true or false if distance is greater than or less than euclidian distance
-    return e_dist <= distance
+    // Check if the calculated distance between two points is within the queried distance
+    calculated_dist <= distance
   }
 
   def runRangeQuery(spark: SparkSession, arg1: String, arg2: String): Long = {
@@ -54,7 +54,7 @@ object SpatialQuery extends App{
     pointDf.createOrReplaceTempView("point")
 
     // YOU NEED TO FILL IN THIS USER DEFINED FUNCTION
-    spark.udf.register("ST_Contains",(queryRectangle:String, pointString:String)=>((ST_Contains(queryRectangle, pointString))))
+    spark.udf.register("ST_Contains",(queryRectangle:String, pointString:String)=>(ST_Contains(queryRectangle, pointString)))
 
     val resultDf = spark.sql("select * from point where ST_Contains('"+arg2+"',point._c0)")
     resultDf.show()
@@ -71,7 +71,7 @@ object SpatialQuery extends App{
     rectangleDf.createOrReplaceTempView("rectangle")
 
     // YOU NEED TO FILL IN THIS USER DEFINED FUNCTION
-    spark.udf.register("ST_Contains",(queryRectangle:String, pointString:String)=>((ST_Contains(queryRectangle, pointString))))
+    spark.udf.register("ST_Contains",(queryRectangle:String, pointString:String)=>(ST_Contains(queryRectangle, pointString)))
 
     val resultDf = spark.sql("select * from rectangle,point where ST_Contains(rectangle._c0,point._c0)")
     resultDf.show()
@@ -85,7 +85,7 @@ object SpatialQuery extends App{
     pointDf.createOrReplaceTempView("point")
 
     // YOU NEED TO FILL IN THIS USER DEFINED FUNCTION
-    spark.udf.register("ST_Within",(pointString1:String, pointString2:String, distance:Double)=>((ST_Within(pointString1, pointString2, distance))))
+    spark.udf.register("ST_Within",(pointString1:String, pointString2:String, distance:Double)=>(ST_Within(pointString1, pointString2, distance)))
 
     val resultDf = spark.sql("select * from point where ST_Within(point._c0,'"+arg2+"',"+arg3+")")
     resultDf.show()
@@ -102,7 +102,7 @@ object SpatialQuery extends App{
     pointDf2.createOrReplaceTempView("point2")
 
     // YOU NEED TO FILL IN THIS USER DEFINED FUNCTION
-    spark.udf.register("ST_Within",(pointString1:String, pointString2:String, distance:Double)=>((ST_Within(pointString1, pointString2, distance))))
+    spark.udf.register("ST_Within",(pointString1:String, pointString2:String, distance:Double)=>(ST_Within(pointString1, pointString2, distance)))
     val resultDf = spark.sql("select * from point1 p1, point2 p2 where ST_Within(p1._c0, p2._c0, "+arg3+")")
     resultDf.show()
 
